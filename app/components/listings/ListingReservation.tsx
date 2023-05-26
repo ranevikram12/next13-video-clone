acc9be7dde2bfd4b9090dc0ba1497f5b00508136
ranevikram12 <102378225+ranevikram12@users.noreply.github.com>
2023-05-27 03:22:15 +0530

    Added time module ui

--- app/components/listings/ListingReservation.tsx
+++ app/components/listings/ListingReservation.tsx
@@ -53,6 +53,43 @@ const ListingReservation: React.FC<
           onChangeDate(value.selection)}
       />
       <hr />
+
+
+      <div className="mt-2 ml-3 p-5 w-40 bg-white rounded-lg shadow-xl">
+  <div className="flex">
+    <select name="hours" className="bg-transparent text-xl appearance-none outline-none">
+      <option value="1">1</option>
+      <option value="2">2</option>
+      <option value="3">3</option>
+      <option value="4">4</option>
+      <option value="5">5</option>
+      <option value="6">6</option>
+      <option value="7">7</option>
+      <option value="8">8</option>
+      <option value="9">9</option>
+      <option value="10">10</option>
+      <option value="11">10</option>
+      <option value="12">12</option>
+    </select>
+    <span className="text-xl mr-3 ">:</span>
+    <select name="minutes" className="bg-transparent text-xl appearance-none outline-none mr-4">
+      <option value="0">00</option>
+      <option value="10">10</option>
+      <option value="20">20</option>
+      <option value="30">30</option>
+      <option value="40">40</option>
+      <option value="50">50</option>
+
+     
+    </select>
+    <select name="ampm" className="bg-transparent text-xl appearance-none outline-none">
+      <option value="am">AM</option>
+      <option value="pm">PM</option>
+    </select>
+  </div>
+</div>
+
+
       <div className="p-4">
         <Button 
           disabled={disabled} 
